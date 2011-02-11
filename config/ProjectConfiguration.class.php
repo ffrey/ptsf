083a2ce4ccc183ad0610a6ce975d3c3dcebc762c
unknown <FFreyssenge@.(none)>
2011-02-11 19:01:08 +0100

    Done

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'C://wamp//lib//sf14//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
+    $this->enablePlugins('sfJqueryReloadedPlugin');
+    $this->enablePlugins('sfAdminDashPlugin');
+  }
+}
